d34c2f5004fd5581931ef5b42ab872f4fe2bfd0c
Jonathan East <jonathan_east@ymail.com>
2020-10-05 17:21:33 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,6 @@ class SessionsController < ApplicationController
   def create
     session[:name] = params[:name]
     !params[:name] || params[:name].empty? ? redirect_to '/login' : redirect_to '/'
-      redirect_to '/login'
-    else
-      redirect_to '/'
-    end
   end
 
   def destroy
